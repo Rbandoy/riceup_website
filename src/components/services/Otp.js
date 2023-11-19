6a5f130782b15d26234cef630026316180c33d02
phrean <reneboy.andoy@e-horizon.com>
2023-11-19 18:59:36 +0800

    change ip

--- src/components/services/Otp.js
+++ src/components/services/Otp.js
@@ -1,9 +1,9 @@
 import {httpClientRequest} from "../../helpers/httpClientRequest" 
 export const OtpService = {
   async sendVerification(phone) {
-    return await httpClientRequest.post(`https://riceup.store/ers/api/v1/subscriber/number_verification/${phone}`);
+    return await httpClientRequest.post(`https://secure.riceup.store/ers/api/v1/subscriber/number_verification/${phone}`);
   },
   async verifyCode(phone, code) {
-    return await httpClientRequest.post(`https://riceup.store/ers/api/v1/subscriber/verify_code/${phone}/${code}`); 
+    return await httpClientRequest.post(`https://secure.riceup.store/ers/api/v1/subscriber/verify_code/${phone}/${code}`); 
   },
 } 
